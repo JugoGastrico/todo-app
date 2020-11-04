0f550140b8b512ca1ff4bc05fa68c3b18d2b46f7
Alexander <alexandertisko@hotmail.com>
2020-11-04 08:14:32 -0300

    as

---
+++ OneDrive/Documents/todo-app/server.js
@@ -0,0 +1,67 @@
+let express = require('express') 
+let mongodb = require('mongodb')
+let app = express()
+let db
+let connectionString = 'mongodb+srv://todoAppUser:motita2812@cluster0.ojdwb.mongodb.net/ToDoApp?retryWrites=true&w=majority' //Acá le decimos a dónde queremos que Mongo se conecte
+mongodb.connect(connectionString, {useNewUrlParser: true, useUnifiedTopology: true}, function(err, client) {
+    db = client.db() //está conectado con la variable let db
+    app.listen(3000)
+}) // a=connection string; b= un objeto de Mongo; c= le damos una función que conectará para que funcione la conexión;
+app.use(express.urlencoded({extended: false})) //esto se agrega siempre en express
+app.get('/', function(req, res) {
+    res.send(`<!DOCTYPE html>
+    <html>
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Simple To-Do App</title>
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+    </head>
+    <body>
+      <div class="container">
+        <h1 class="display-4 text-center py-1">To-Do App</h1>
+        
+        <div class="jumbotron p-3 shadow-sm">
+          <form action="/create-item" method="POST">
+            <div class="d-flex align-items-center">
+              <input name="item" autofocus autocomplete="off" class="form-control mr-3" type="text" style="flex: 1;">
+              <button class="btn btn-primary">Add New Item</button>
+            </div>
+          </form>
+        </div>
+        
+        <ul class="list-group pb-5">
+          <li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
+            <span class="item-text">Fake example item #1</span>
+            <div>
+              <button class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
+              <button class="delete-me btn btn-danger btn-sm">Delete</button>
+            </div>
+          </li>
+          <li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
+            <span class="item-text">Fake example item #2</span>
+            <div>
+              <button class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
+              <button class="delete-me btn btn-danger btn-sm">Delete</button>
+            </div>
+          </li>
+          <li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
+            <span class="item-text">Fake example item #3</span>
+            <div>
+              <button class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
+              <button class="delete-me btn btn-danger btn-sm">Delete</button>
+            </div>
+          </li>
+        </ul>
+        
+      </div>
+      
+    </body>
+    </html>`)
+})
+app.post('/create-item', function (req, res) {
+    db.collection('items').insertOne({text: req.body.item}, function() {
+        res.send("Hola") 
+    }) 
+})
+//acá saqué el app.listen
